8fce52514d45ff29d572c1513a28fd7011f64193
linus <mail@linusgenz.dev>
2025-01-06 15:07:31 +0100

    fix month today api

--- src/app/api/today/route.ts
+++ src/app/api/today/route.ts
@@ -3,12 +3,13 @@ import {ReqWeatherData, WeatherDataArray} from "@/app/types/WeatherData";
 export async function GET() {
     const date = new Date();
 
-    const dateString = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" +
+    const dateString = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, '0') + "-" +
         date.getDate().toString().padStart(2, '0');
 
     const res = await fetch(
         process.env.URL + `/history?date=${dateString}`,
     );
+    console.log(process.env.URL + `/history?date=${dateString}`)
     const data: ReqWeatherData[] = await res.json();
     if (data[0].error) {
 
